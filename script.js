ed7a2bf4e0bd132903f843c2cb09dd32c07320f6
William Fernandes <wiilfern1910@outlook.com>
2023-05-03 11:55:37 -0300

    Animations

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ function menuOpen() {
   burguer.classList.toggle("animation-rotator");
   burguer.classList.toggle("animation-rotatereverse");
 }
+
+var typed1 = new Typed(".typed1", {
+  strings: ["CYBERTOWN"],
+  loop: false,
+  showCursor: false,
+  typeSpeed: 140,
+});
+
+var typed2 = new Typed(".typed2", {
+  strings: ["NightCity...", "Hell..."],
+  loop: true,
+  typeSpeed: 140,
+  backSpeed: 80,
+});
