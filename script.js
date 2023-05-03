0f66e65f4e9fd2d69c7c07505fd8e98abee6dca4
William Fernandes <wiilfern1910@outlook.com>
2023-05-03 13:18:25 -0300

    Menu Fix

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-let burguer = document.getElementById("menu-burguer");
 
+
+// Menu Mobile com Keyframes
+
+let burguer = document.getElementById("menu-burguer");
+let counter = 0;
 burguer.addEventListener("click", menuOpen);
 
 function menuOpen() {
+  counter++;
   let menuMobile = document.getElementsByTagName("nav")[0];
   menuMobile.classList.toggle("menu-mobile");
-  burguer.classList.toggle("animation-rotator");
-  burguer.classList.toggle("animation-rotatereverse");
+
+  if (counter == 1) {
+    burguer.classList.remove("animation-join");
+    burguer.classList.toggle("animation-rotate");
+  }
+
+  if (counter == 2) {
+    burguer.classList.remove("animation-rotate");
+    burguer.classList.toggle("animation-rotaterev");
+  }
+
+  if (counter > 2) {
+    burguer.classList.toggle("animation-rotaterev");
+    burguer.classList.toggle("animation-rotate");
+    counter = 1;
+  }
 }
+
+
+
+// Efeitos com Typed.js
 
 var typed1 = new Typed(".typed1", {
   strings: ["CYBERTOWN"],
