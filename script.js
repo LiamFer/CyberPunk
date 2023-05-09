0615de59e8139fb486d6bbbf47303276b2f984ef
William Fernandes <wiilfern1910@outlook.com>
2023-05-09 13:36:20 -0300

    Correção

--- script.js
+++ script.js
@@ -41,9 +41,12 @@ var typed3 = new Typed(".typed3", {
   typeSpeed: 140,
   showCursor: false,
 });
+
 var typed4 = new Typed(".typed4", {
-  strings: ["In the dystopian world of Cyberpunk 2077, where corporations hold ultimate power and the line between human and machine is blurred, the words giving up hold a different weight. Our greatest weakness as individuals, as a society, and as a species, lies in surrendering to the oppression of those in control."],
+  strings: [
+    "In the dystopian world of Cyberpunk 2077, where corporations hold ultimate power and the line between human and machine is blurred, the words giving up hold a different weight. Our greatest weakness as individuals, as a society, and as a species, lies in surrendering to the oppression of those in control.",
+  ],
   loop: false,
-  typeSpeed: 40,
+  typeSpeed: 10,
   showCursor: false,
 });
